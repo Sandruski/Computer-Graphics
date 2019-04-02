bb8da6d48c7e275be348afd4be5915aad4530f32
Sandra Alvarez <sandruskiag@gmail.com>
2019-04-02 09:37:20 +0200

    Hello World!

---
+++ ComputerGraphics/JavascriptTests/file.js
@@ -0,0 +1,13 @@
+class MyClass
+{
+    constructor(attribute2) 
+    {
+        this.attribute1 = 0;
+        this.attribute2 = attribute2;
+    }
+
+exampleMethod() 
+    {
+        document.write(this.attribute2);
+    }
+}
